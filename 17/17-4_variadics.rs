c99f7d518f76666798f1a0baae8e12e224f0c0ee
kdnakt <a.kid.1985@gmail.com>
2022-10-27 00:20:55 +0900

    20221026 17-2..17-4 macro

---
+++ 17/17-4_variadics.rs
@@ -0,0 +1,27 @@
+macro_rules! calculate {
+    // 単一の`eval`のためのパターン
+    // The pattern for a single `eval`
+    (eval $e:expr) => {{
+        {
+            let val: usize = $e; // Force types to be integers
+            println!("{} = {}", stringify!{$e}, val);
+        }
+    }};
+
+    // 複数の`eval`を再帰的に分解する
+    // Decompose multiple `eval`s recursively
+    (eval $e:expr, $(eval $es:expr),+) => {{
+        calculate! { eval $e }
+        calculate! { $(eval $es),+ }
+    }};
+}
+
+fn main() {
+                 // ほら！可変な`calculate!`だよ！
+    calculate! { // Look ma! Variadic `calculate!`!
+        eval 1 + 2,
+        eval 3 + 4,
+        eval (2 * 3) + 1,
+        eval 100 / 20
+    }
+}
