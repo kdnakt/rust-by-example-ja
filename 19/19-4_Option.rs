177cc8c7ff00ecb17e680a502043daacd1f938a2
kakichi_y <kakichi_y@works-hi.co.jp>
2023-02-01 19:43:22 +0900

    19.4 Option

--- 19/19-4_Option.rs
+++ 19/19-4_Option.rs
@@ -1,52 +1,47 @@
+// An integer division that doesn't `panic!`
+// `panic!`を起こさない整数の割り算
+fn checked_division(dividend: i32, divisor: i32) -> Option<i32> {
+    if divisor == 0 {
+        // Failure is represented as the `None` variant
+        // 失敗は`None`としてあらわされる。
+        None
+    } else {
+        // Result is wrapped in a `Some` variant
+        // 結果は`Some`にラップされる。
+        Some(dividend / divisor)
+    }
+}
+
+// This function handles a division that may not succeed
+// この関数は失敗する割り算を扱うことができる
+fn try_division(dividend: i32, divisor: i32) {
+    // `Option` values can be pattern matched, just like other enums
+    // `Option` の値は、他のあらゆる列挙型と同様パターンマッチに使用できる。
+    match checked_division(dividend, divisor) {
+        None => println!("{} / {} failed!", dividend, divisor),
+        Some(quotient) => {
+            println!("{} / {} = {}", dividend, divisor, quotient)
+        },
+    }
+}
+
 fn main() {
-    // (all the type annotations are superfluous)
-    // A reference to a string allocated in read only memory
-    // （以下の例では型を明示していますが、これらは必須ではありません。）
-    // read only memory上に割り当てられた文字列への参照
-    let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
-    println!("Pangram: {}", pangram);
+    try_division(4, 2);
+    try_division(1, 0);
 
-    // Iterate over words in reverse, no new string is allocated
-    // 単語を逆順にイテレートする。新しい文字列の割り当ては起こらない。
-    println!("Words in reverse");
-    for word in pangram.split_whitespace().rev() {
-        println!("> {}", word);
-    }
+    // Binding `None` to a variable needs to be type annotated
+    // `None`を変数にアサインする際は、型を明示しなくてはならない。
+    let none: Option<i32> = None;
+    let _equivalent_none = None::<i32>;
 
-    // Copy chars into a vector, sort and remove duplicates
-    // 文字をベクトルにコピー。ソートして重複を除去
-    let mut chars: Vec<char> = pangram.chars().collect();
-    chars.sort();
-    chars.dedup();
-    println!("chars: {:?}", chars);
+    // let optional_float = Some(0f32);
+    let optional_float: Some = Some(0.0); // default f64
 
-    // Create an empty and growable `String`
-    // 中身が空で、伸長可能な`String`を作成
-    let mut string = String::new();
-    for c in chars {
-        // Insert a char at the end of string
-        // 文字を文字列の末端に挿入
-        string.push(c);
-        // Insert a string at the end of string
-        // 文字列を文字列の末端に挿入
-        string.push_str(", ");
-    }
+    // Unwrapping a `Some` variant will extract the value wrapped.
+    // `Some`をアンラップすると中の値を取得できる。
+    println!("{:?} unwraps to {:?}", optional_float, optional_float.unwrap());
 
-    // The trimmed string is a slice to the original string, hence no new
-    // allocation is performed
-    // 文字列のトリミング（特定文字種の除去）はオリジナルの文字列のスライスを
-    // 返すので、新規のメモリ割り当ては発生しない。
-    let chars_to_trim: &[char] = &[' ', ','];
-    let trimmed_str: &str = string.trim_matches(chars_to_trim);
-    println!("Used characters: {}", trimmed_str);
-
-    // Heap allocate a string
-    // 文字列をヒープに割り当てる。
-    let alice = String::from("I like dogs");
-    // Allocate new memory and store the modified string there
-    // 新しくメモリを確保し、変更を加えた文字列をそこに割り当てる。
-    let bob: String = alice.replace("dog", "cat");
-
-    println!("Alice says: {}", alice);
-    println!("Bob says: {}", bob);
+    // Unwrapping a `None` variant will `panic!`
+    // `None`をアンラップしようとすると`panic!`る
+    println!("{:?} unwraps to {:?}", none, none.unwrap());
 }
